2a3b946a01467999f88254e01a6992baf8b85be0
Derya Subaşı <78120692+derya12@users.noreply.github.com>
2021-11-29 02:12:45 +0300

    Create indexx.php

---
+++ ARAMA/indexx.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>www.search.com</title>
+	<link href="style.css" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+		<div class="main">
+		<img src="Camera Roll/logo4.png" id="logo">
+		
+		<form id="form" method="GET" action="ara.php">
+			<input type="text" name="q" id="inputtext" required/><br><br>
+			<input type="submit" name="Ara" value="ARA" id="btn"/>
+</form>
+	</div>
+
+</body>
+</html>
